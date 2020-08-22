daed4bd4c26cd99d7b8832f675aa90ed78c017a9
Prajwol <prajwolshrestha08@gmail.com>
2020-08-22 09:47:07 +0545

    Number_guessing

--- Number_guessing.py
+++ Number_guessing.py
@@ -4,8 +4,8 @@ i = 1
 while i <= 5 :
 
 	attempts = 5
-	guess = input("Enter your guess: ")
-	number = random.randint(0,1)
+	guess = input("Enter a number between 1 to 5: ")
+	number = random.randint(1,5)
 	print(number)
 	if number == int(guess) and attempts != 0:
 		print("Your guess "+ str(number) + " is correct.")
@@ -14,8 +14,8 @@ while i <= 5 :
 	 	print("Your guess was incorrect.")
 	 	print("\n")
 	 	attempts = attempts - i 
-	 	print("You still have " + str(attempts) + " attempts left.")
-	elif number != guess and attempts == 0:
+	 	print("You have " + str(attempts) + " attempts left.")
+	elif number != guess and attempts == 0 :
 	 	print("Your guess was incorrect.")
 	 	print("\n")
 	 	print("You are out of attempts")	 	
